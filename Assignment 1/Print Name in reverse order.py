4aa046b181d7816634500b913c08e2c05699cb39
Puneet Srivastava <srivpuneet01@gmail.com>
2018-09-24 03:09:18 +0530

    first commit

---
+++ Assignment 1/Print Name in reverse order.py
@@ -0,0 +1,6 @@
+#Ask for first name
+firstName = input("Please enter your first name:")
+#Ask for last name
+lastName = input("Please enter your last name:")
+#Print the output in reverse order
+print("Your name is: " + lastName + " "+ firstName)
---
+++ Assignment 1/Volume of the sphere.py
@@ -0,0 +1,7 @@
+#import math package for mathematical operations
+import math
+
+diameter = 12
+#compute the volume by applying the formula
+volume = (4/3)* math.pi * math.pow(diameter/2, 3)
+print(volume)
