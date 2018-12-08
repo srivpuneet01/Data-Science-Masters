18862d4bc0b53de45b736c223e73313719d7eaf3
Puneet Srivastava <srivpuneet01@gmail.com>
2018-12-08 23:52:02 +0530

    Assignment 6

---
+++ Assignment 6/vander.py
@@ -0,0 +1,19 @@
+#import numpy module
+import numpy as np
+
+def generate_vander_matrix(vector, no_of_columns, increasing):
+    #define numpy array as input
+    x=np.array(vector)
+    #Show vander array out put Method1
+    # print(np.vander(vector, no_of_columns, increasing=increasing))
+    #show Vander array out with Method2
+    start, stop, step = 0, 0, 1
+    if not increasing:
+        stop = no_of_columns
+    else:
+        start, stop, step = no_of_columns - 1, -1, -1
+    print("\n")
+    print("Printing in {a} order:".format(a="increasing" if increasing else "decreasing"))
+    print(np.column_stack([x**(no_of_columns - 1 - i) for i in range(start, stop, step)]))
+
+generate_vander_matrix([1,2,3,4,5], 5, True)
